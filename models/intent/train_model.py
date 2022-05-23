3ed6a3a8f742d4175b066a82f99848622f882ea5
PatrickJaeWon <patrick7798@naver.com>
2022-05-23 16:36:42 +0900

    모델생성 및 의도예측까지

--- models/intent/train_model.py
+++ models/intent/train_model.py
@@ -34,3 +34,71 @@ padded_seqs = preprocessing.sequence.pad_sequences(sequences, maxlen=MAX_SEQ_LEN
 
 print(padded_seqs.shape)
 print(len(intents))
+
+# 학습용, 검증, 테스트 7:2:1
+ds = tf.data.Dataset.from_tensor_slices((padded_seqs, intents))
+ds = ds.shuffle(len(queries)) # 질문지 섞기
+
+train_size = int(len(padded_seqs) * 0.7)
+val_size = int(len(padded_seqs) * 0.2)
+test_size = int(len(padded_seqs) * 0.1)
+
+train_ds = ds.take(train_size).batch(20)
+val_ds = ds.skip(train_size).take(val_size).batch(20)
+test_ds = ds.skip(train_size + val_size).take(test_size).batch(20)
+
+# 하이퍼 파라미터 설정
+dropout_prob=0.5
+EMB_SIZE=128
+EPOCH=5
+VOCAB_SIZE=len(p.word_index) +1 # 전체 단어갯수
+
+# CNN 모델정의
+input_layer = Input(shape=(MAX_SEQ_LEN,))
+embedding_layer = Embedding(VOCAB_SIZE, EMB_SIZE, input_length=MAX_SEQ_LEN)(input_layer)
+dropout_emb = Dropout(rate=dropout_prob)(embedding_layer)
+
+conv1 = Conv1D(
+    filters=128, # 평탄화
+    kernel_size=3,
+    padding='valid',
+    activation=tf.nn.relu)(dropout_emb)
+pool1 = GlobalMaxPool1D()(conv1)
+
+conv2 = Conv1D(
+    filters=128, # 평탄화
+    kernel_size=4,
+    padding='valid',
+    activation=tf.nn.relu)(dropout_emb)
+pool2 = GlobalMaxPool1D()(conv2) #1D 시간 데이터에 대해서 전역 최대 폴링 작업
+
+conv3 = Conv1D(
+    filters=128, # 평탄화
+    kernel_size=5,
+    padding='valid',
+    activation=tf.nn.relu)(dropout_emb)
+pool3 = GlobalMaxPool1D()(conv3)
+
+# 3,4,5gram 이후 합치기-> (e.g. 하이퍼파라미터 설정, 모델 정의, 3,4,5-gram 이후 합치기)
+concat = concatenate([pool1, pool2, pool3])
+
+hidden = Dense(128, activation=tf.nn.relu)(concat)
+dropout_hidden = Dropout(rate=dropout_prob)(hidden) #과적합 방지
+#출력
+logits = Dense(5, name='logits')(dropout_hidden) #5가지 클래스를 기준으로 의도 분석 하였으므로 출력노드도 5개
+predictions = Dense(5, activation=tf.nn.softmax)(logits) # 가장 큰 점수(score)가 높은 클래스가 예측값이 됨
+
+# 모델 생성
+model=Model(inputs=input_layer,outputs=predictions)
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# 모델 학습
+model.fit(train_ds, validation_data=val_ds, epochs=EPOCH, verbose=1)
+
+# 모델 평가(테스트 데이터셋 이용)
+loss, accuracy=model.evaluate(test_ds, verbose=1)
+print('Accuracy: %f' % (accuracy * 100))
+print('loss: %f' % (loss))
+
+# 모델 저장
+model.save('intent_model.h5')
